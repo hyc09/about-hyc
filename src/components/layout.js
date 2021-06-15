e0cdb2b3abeb021be6067d371d4188d979d63433
chiang <cyakn2001@gmail.com>
2021-06-15 21:05:54 +0800

    update metadata

--- src/components/layout.js
+++ src/components/layout.js
@@ -41,8 +41,8 @@ const Layout = ({ children, location }) => {
           <Helmet
             title={data.site.siteMetadata.title}
             meta={[
-              { name: 'description', content: 'Sample' },
-              { name: 'keywords', content: 'sample, something' },
+              { name: 'description', content: '個人網站' },
+              { name: 'keywords', content: '作品集、個人簡介' },
             ]}
           >
             <html lang="en" />
